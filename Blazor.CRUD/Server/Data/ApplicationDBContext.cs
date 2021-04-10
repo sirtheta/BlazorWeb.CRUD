9cb071f2f023117194cad2a7c672fd8e56ae505f
sirtheta <neo38@bluewin.ch>
2021-04-10 19:19:46 +0200

    Add project files.

---
+++ Blazor.CRUD/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Blazor.CRUD.Shared.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor.CRUD.Server.Data
+{
+	public class ApplicationDBContext : DbContext
+	{
+		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+		{
+		}
+		public DbSet<Developer> Developers { get; set; }
+	}
+}
---
+++ Blazor.CRUD/Shared/Model/Developer.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor.CRUD.Shared.Model
+{
+	public class Developer
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public decimal Experience { get; set; }
+	}
+}
